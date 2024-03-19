f065f6ed66dba9161c9898f0460dbb0e8b2c7278
alice20240103 <alice20240103@gmail.com>
2024-03-19 10:14:40 +0900

    setter, getter 추가 [김사자]

--- Rect.java
+++ Rect.java
@@ -1,6 +1,21 @@
 class Rect{
 	int width;
 	int height;
+	public void setWidth(int width){
+		this.width = width;
+	}
+	public void setHeight(int height){
+		this.height = height;
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+
+	public int getHeight(){
+		return height;
+	}
+
 	public int getArea(){
 		return width * height;
 	}
